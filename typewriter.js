f9930664e7033f222a234095647f202946f23de9
Chibu <atuchibueze@gmail.com>
2022-04-12 15:33:40 -0400

    created an animated sentence using setTimeout and process.stedout.write

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+
+let timer = 0
+for (let i = 0; i < sentence.length; i++) {
+  timer += 50;
+  setTimeout(() => {
+    if (i === sentence.length - 1) {
+      process.stdout.write(sentence[i] + "\n");
+    } else {
+      process.stdout.write(sentence[i]);
+    }
+  }, timer)
+}
+
+
+// console.log("\n")
